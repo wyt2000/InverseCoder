fe9541e260caabe412615a26c39b6a1ba2b39704
wyt2000 <wytwyt200@gamil.com>
2024-03-11 11:02:43 +0800

    fix: format in merge 2 files.

--- LLMSampler/scripts/merge_evol_and_summary.py
+++ LLMSampler/scripts/merge_evol_and_summary.py
@@ -1,14 +1,14 @@
 import jsonlines                                                                                       
-                                                                                                       
-with open('magicoder_data/codevol-wizardcoder-summary-0305.jsonl') as f:                               
-	instructions = [eval(line)['response'].strip() for line in f.readlines()]                      
-with open('magicoder_data/codevol-wizardcoder-0304.jsonl') as f:                                       
-	responses = [eval(line)['response'].strip() for line in f.readlines()]                         
-                                                                                                       
-with jsonlines.open('magicoder_data/codevol-wizardcoder-instruction-data-0305.jsonl', mode='w') as writer:                                                                                                    
-	for inst, resp in zip(instructions, responses):                                                    
-		line = {                                                                                       
-			'instruction' : f'Write code that satisfies the following purposes:\n{inst}',              
-			'response' : resp                                                                          
-		}                                                                                              
-	writer.write(line)                                                                             
+
+with open('magicoder_data/oss-instruct-codevol-reversed-wizardcoder-gpt4-summary-0310.jsonl') as f:
+	instructions = [eval(line)['response'].strip() for line in f.readlines()]
+with open('magicoder_data/oss-instruct-codevol-0306.jsonl') as f:
+	responses = [eval(line)['response'].strip() for line in f.readlines()]
+
+with jsonlines.open('magicoder_data/oss-instruct-codevol-0306-summary-by-wizardcoder-3200-0310.jsonl', mode='w') as writer:
+	for inst, resp in zip(instructions, responses):
+		line = {
+			'instruction' : inst,
+			'response' : resp
+		}
+		writer.write(line)
