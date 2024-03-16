b5d90e765ca6e09f96b6112100d4848af27a54c8
wyt2000 <wyt200200@gmail.com>
2024-03-16 10:51:34 +0800

    feat: add combined data clean and requirements.txt

--- LLMSampler/scripts/clean_data.py
+++ LLMSampler/scripts/clean_data.py
@@ -1,31 +1,41 @@
 # Remove bad data
 import jsonlines
 
-with open('magicoder_data/codevol-wizardcoder-0304-instruct-by-wizardcoder-reversed-0306-0311-cleaned.jsonl') as f:
-    lines = list(f.readlines())
+with open('magicoder_data/oss-instruct-codevol-0306-no-prompt-instruction-data-cleaned.jsonl') as f:
+    no_prompt_lines = list(f.readlines())
+
+with open('magicoder_data/oss-instruct-codevol-0306-problem-prompt-instruction-data-cleaned.jsonl') as f:
+    problem_prompt_lines = list(f.readlines())
+
 
 def check(inst, resp):
     s = ''.join([inst, resp])
-    # if 'OCR' in inst or 'apologize' in s or 'apologies' in s or 'apology' in s:
-    if 'apologize' in s or 'apologies' in s or 'apology' in s:
+    if 'OCR' in inst:
         return False
+    # if 'apologize' in s or 'apologies' in s or 'apology' in s:
+    #    return False
+    #if '```' in inst:
+    #    return False
     return True
 
 def redecode(s):
     return s.encode('utf-8', 'backslashreplace').decode('utf-8')
 
-with jsonlines.open('magicoder_data/codevol-wizardcoder-0304-instruct-by-wizardcoder-reversed-0306-0311-cleaned-selected.jsonl', mode='w') as writer:
+with jsonlines.open('magicoder_data/oss-instruct-codevol-0306-problem-prompt-instruction-data-cleaned-selected.jsonl', mode='w') as writer:
     removed_data = []
-    for line in lines:
-        line = eval(line)
-        line['instruction'] = redecode(line['instruction'])
-        line['response'] = redecode(line['response'])
-        if check(line['instruction'], line['response']):
-            writer.write(line)
+    for no_prompt_line, problem_prompt_line in zip(no_prompt_lines, problem_prompt_lines):
+        no_prompt_line = eval(no_prompt_line)
+        no_prompt_line['instruction'] = redecode(no_prompt_line['instruction'])
+        no_prompt_line['response'] = redecode(no_prompt_line['response'])
+        problem_prompt_line = eval(problem_prompt_line)
+        problem_prompt_line['instruction'] = redecode(problem_prompt_line['instruction'])
+        problem_prompt_line['response'] = redecode(problem_prompt_line['response'])
+        if check(no_prompt_line['instruction'], no_prompt_line['response']):
+            writer.write(problem_prompt_line)
         else:
-            removed_data.append(line)
+            removed_data.append(problem_prompt_line)
 
-with jsonlines.open('magicoder_data/codevol-wizardcoder-0304-instruct-by-wizardcoder-reversed-0306-0311-removed.jsonl', mode='w') as writer:
+with jsonlines.open('magicoder_data/oss-instruct-codevol-0306-problem-prompt-instruction-data-cleaned-removed.jsonl', mode='w') as writer:
     for line in removed_data:
         writer.write(line)
 
