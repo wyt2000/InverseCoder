bacd4768ae6140dbc09dd583b261b192aa2bf7aa
wyt2000 <wyt200200@gmail.com>
2024-03-22 11:16:42 +0800

    feat: add python files in func_name_detect.

--- deduplication/func_name_detect.py
+++ deduplication/func_name_detect.py
@@ -14,12 +14,39 @@ with open('magicoder_data/humaneval-benchmark.jsonl') as f:
             if isinstance(node, ast.FunctionDef):
                 func_names[node.name] = 0
 
+def _check_node(node):
+    if not node.body:
+        return False
+    if len(node.body) > 2:
+        return True
+    return not all(isinstance(x, (ast.Pass, ast.Str, ast.Raise)) for x in node.body)
+
+def check_code_implementation(code):
+    root = ast.parse(code)
+    for node in ast.walk(root):
+        if isinstance(node, (ast.FunctionDef, ast.ClassDef)):
+            if not _check_node(node):
+                return False
+    return True
+
+def extract_code(code: str):
+    if not '```' in code:
+        return code
+    start = code.find('```')
+    end = code.rfind('```')
+    ret = code[start:end]
+    ret = '\n'.join(ret.splitlines()[1:])
+    if not ret:
+        ret = code[start:]
+        ret = '\n'.join(ret.splitlines()[1:])
+    return ret
+
 def redecode(s):
     return s.encode('utf-8', 'backslashreplace').decode('utf-8')
 
 data_path = 'magicoder_data/data-evol_instruct-decontaminated.jsonl'
 fixed_dataset = []
-with jsonlines.open(f'{data_path}.duplicated', mode='w') as writer:
+with jsonlines.open(f'{data_path}.python_and_duplicated', mode='w') as writer:
     with open(data_path) as f:
         dataset = list(f.readlines())
         with tqdm.tqdm(total=len(dataset)) as pbar:
@@ -29,16 +56,28 @@ with jsonlines.open(f'{data_path}.duplicated', mode='w') as writer:
                     'instruction': redecode(line['instruction']),
                     'response': redecode(line['response']
                 )}
+                inst = line['instruction']
+                resp = line['response']
                 for name in func_names.keys():
-                    inst = line['instruction']
-                    resp = line['response']
                     pattern = f'def {name}('
                     if pattern in inst: #or pattern in resp:
                         writer.write(data)
                         func_names[name] += 1
                         break
                 else:
-                    fixed_dataset.append(data)
+                    code = extract_code(resp)
+                    try:
+                        if not code:
+                            raise ValueError('Empty code!')
+                        ast.parse(code)
+                        writer.write(data)
+                    except Exception as err:
+                        #print(line['response'])
+                        #print(code)
+                        #print(err)
+                        pass
+                    finally:
+                        fixed_dataset.append(data)
                 pbar.update(1)
 with jsonlines.open(f'{data_path}.fixed', mode='w') as writer:
     for data in fixed_dataset:
