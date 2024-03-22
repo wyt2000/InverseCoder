24974cf76139f5fb129790e9d905e126d5b3abbc
wyt2000 <wytwyt200@gmail.com>
2024-03-22 12:34:22 +0800

    fix: shuffle_data.

--- LLMSampler/scripts/shuffle_data.py
+++ LLMSampler/scripts/shuffle_data.py
@@ -3,10 +3,15 @@ import pandas as pd
 import jsonlines
 random.seed(42)
 
-with open('magicoder_data/data-evol_instruct-decontaminated.jsonl') as f:
+def redecode(s):
+    return s.encode('utf-8', 'backslashreplace').decode('utf-8')
+
+with open('magicoder_data/starcoderdata_evol_with_python_and_dup_data_0322.jsonl') as f:
 	lines = list(f.readlines())
-    lines = random.shuffle(lines)
-	with jsonlines.open('magicoder_data/data-evol_instruct-decontaminated-shuffled.jsonl', mode='w') as writer:
+	random.shuffle(lines)
+	with jsonlines.open('magicoder_data/starcoderdata_evol_with_python_and_dup_data_0322.jsonl.shuffled', mode='w') as writer:
 		for line in lines:
 			line = eval(line)
+			line['instruction'] = redecode(line['instruction'])
+			line['response'] = redecode(line['response'])
 			writer.write(line)
