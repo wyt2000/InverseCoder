0fb272685a099a16c2f5c7a5a7bdb554fec720e6
wyt2000 <wyt200200@gmail.com>
2024-03-25 19:32:13 +0800

    feat: add TestGen.

--- LLMSampler/scripts/show_jsonl.py
+++ LLMSampler/scripts/show_jsonl.py
@@ -4,7 +4,7 @@ input_path = 'magicoder_data/data-evol_instruct-decontaminated.jsonl.fixed.no_py
 with open(input_path) as f:
     for line in list(f.readlines()):
         line = eval(line)
-        if 'i += 6' not in line['response']: continue
+        if 'prime' not in line['response']: continue
         print('@@instr')
         print(line['instruction'])
         print('@@resp')
