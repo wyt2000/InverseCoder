69ec5be616bfe19b24b504f063be2baf79ada764
wyt2000 <wytwyt200@gmail.com>
2024-07-10 20:34:15 +0800

    remove useless files.

--- src/scripts/show_jsonl.py
+++
@@ -1,9 +0,0 @@
-import json
-input_path = '/lustre/S/wuyt/Repoformer/repo_eval/processed_data/python_function_completion.jsonl'
-
-with open(input_path) as f:
-    for line in list(f.readlines())[:1]:
-        line = json.loads(line)
-        for key, value in line.items():
-            print(f'@@{key}')
-            print(value)
